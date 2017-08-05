41b8ebf342690cdd0ec65320ed0d309a26aa81db
Kenneth Powers <ken@kenpowers.net>
2017-08-05 17:11:55 -0400

    Wire up People container with graphql data.

--- client/src/containers/people.jsx
+++ client/src/containers/people.jsx
@@ -1,5 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { gql, graphql } from 'react-apollo';
+import { branch, renderComponent, compose } from 'recompose';
+import { Div } from 'glamorous';
 
-const People = () => <div><h2>People</h2></div>;
+const peopleQuery = gql`
+  query PeopleQuery {
+    totalPeople
+    people(order: first_name, per_page: 25) {
+      id
+      first_name
+      last_name
+    }
+  }
+`;
 
-export default People;
+const personShape = PropTypes.shape({
+  id: PropTypes.string,
+  first_name: PropTypes.string,
+  last_name: PropTypes.string,
+});
+
+const Person = ({ person }) => (
+  <div>
+    <h3>{person.first_name} {person.last_name}</h3>
+  </div>
+);
+
+Person.propTypes = {
+  person: personShape.isRequired,
+};
+
+const People = ({ data }) => (
+  <div>
+    <h2>People ({data.totalPeople})</h2>
+    {data.people.map(person => <Person key={person.id} person={person} />)}
+  </div>
+);
+
+People.propTypes = {
+  data: PropTypes.shape({
+    totalPeople: PropTypes.integer,
+    people: PropTypes.arrayOf(personShape),
+  }).isRequired,
+};
+
+const placeholder = branch(
+  ({ data }) => data.loading,
+  renderComponent(props => (
+    <Div {...props} backgroundColor="#F3F3F3" textAlign="center" width="100%" height="100%" />
+  )),
+);
+
+export default compose(
+  graphql(peopleQuery),
+  placeholder,
+)(People);
