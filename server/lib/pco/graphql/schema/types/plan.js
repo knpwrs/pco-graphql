de37845af8fac5d0afeee49e8231985e6a769b2f
Kenneth Powers <ken@kenpowers.net>
2017-08-04 21:38:09 -0400

    Add query to retrieve individual plan.

--- server/lib/pco/graphql/schema/types/plan.js
+++ server/lib/pco/graphql/schema/types/plan.js
@@ -1,4 +1,5 @@
 import { makeLinkResolvers, makeAttributeResolvers } from '../utils';
+import { getResourceUrl } from '../../../api';
 
 export const typeDefs = [`
   # A single plan within a Service Type.
@@ -13,9 +14,19 @@ export const typeDefs = [`
     # People scheduled for this plan.
     team_members: [PlanPerson]
   }
+
+  extend type Query {
+    plan(typeId: ID!, planId: ID!): Plan
+  }
 `];
 
 export const resolvers = {
+  Query: {
+    plan(root, { typeId, planId }, { loader }) {
+      const serviceTypeUrl = getResourceUrl('services', 'service_types', typeId);
+      return loader.load(`${serviceTypeUrl}/plans/${planId}`);
+    },
+  },
   Plan: {
     ...makeAttributeResolvers([
       'created_at',
