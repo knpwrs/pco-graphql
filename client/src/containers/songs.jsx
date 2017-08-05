3488a9deb5265e6fa7c0da4f673bb120d97fd41b
Kenneth Powers <ken@kenpowers.net>
2017-08-05 17:24:57 -0400

    Wire up Songs container with graphql data.

--- client/src/containers/songs.jsx
+++ client/src/containers/songs.jsx
@@ -1,50 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route, Link } from 'react-router-dom';
+import { gql, graphql } from 'react-apollo';
+import { branch, renderComponent, compose } from 'recompose';
+import { Div } from 'glamorous';
 
-const Song = ({ match }) => (
+const songsQuery = gql`
+  query SongsQuery {
+    totalSongs
+    songs(order: title, per_page: 25) {
+      id
+      title
+      author
+    }
+  }
+`;
+
+const songShape = PropTypes.shape({
+  id: PropTypes.string,
+  title: PropTypes.string,
+  author: PropTypes.string,
+});
+
+const Song = ({ song }) => (
   <div>
-    <h3>Song: {match.params.songId}</h3>
+    <h3>{song.title} by {song.author}</h3>
   </div>
 );
 
 Song.propTypes = {
-  match: PropTypes.shape({
-    params: PropTypes.shape({
-      songId: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired,
+  song: songShape.isRequired,
 };
 
-const Songs = ({ match }) => (
+const Songs = ({ data }) => (
   <div>
-    <h2>Songs</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/1`}>Fierce</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/2`}>Brighter</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/3`}>Here As In Heaven</Link>
-      </li>
-    </ul>
-    <Route path={`${match.url}/:songId`} component={Song} />
-    <Route
-      exact
-      path={`${match.url}`}
-      render={() => (
-        <h3>Please select a song.</h3>
-      )}
-    />
+    <h2>Songs ({data.totalSongs})</h2>
+    {data.songs.map(song => <Song key={song.id} song={song} />)}
   </div>
 );
 
 Songs.propTypes = {
-  match: PropTypes.shape({
-    url: PropTypes.string.isRequired,
+  data: PropTypes.shape({
+    totalSongs: PropTypes.integer,
+    songs: PropTypes.arrayOf(songShape),
   }).isRequired,
 };
 
-export default Songs;
+const placeholder = branch(
+  ({ data }) => data.loading,
+  renderComponent(props => (
+    <Div {...props} backgroundColor="#F3F3F3" textAlign="center" width="100%" height="100%" />
+  )),
+);
+
+export default compose(
+  graphql(songsQuery),
+  placeholder,
+)(Songs);
