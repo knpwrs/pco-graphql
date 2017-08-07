4f7544fbf41210dea158ea6ca07e6b7adb893894
Kenneth Powers <ken@kenpowers.net>
2017-08-07 01:23:04 -0400

    Initial implementation of splitting by route.

--- client/src/root.jsx
+++ client/src/root.jsx
@@ -2,15 +2,30 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import { css } from 'glamor';
 import { Div } from 'glamorous';
+import loadable from 'react-loadable';
 import ThemeByRouteProvider from './components/theme-by-route-provider';
 import AppHeader from './containers/app-header';
-import Plans from './containers/plans';
-import People from './containers/people';
-import Songs from './containers/songs';
 import Player from './containers/player';
 
 css.global('body', {
   backgroundColor: '#FCFCFC',
+});
+
+const loading = () => <h2>Loading</h2>;
+
+const Plans = loadable({
+  loader: () => import('./containers/plans'),
+  loading,
+});
+
+const People = loadable({
+  loader: () => import('./containers/people'),
+  loading,
+});
+
+const Songs = loadable({
+  loader: () => import('./containers/songs'),
+  loading,
 });
 
 const Root = () => (
