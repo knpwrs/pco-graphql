2042068bb43516ce9661768e365b8e6bbe7179f0
Kenneth Powers <ken@kenpowers.net>
2017-08-07 17:03:53 -0400

    Add French translation.

--- client/src/i18n/index.js
+++ client/src/i18n/index.js
@@ -2,10 +2,11 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './en.yml';
 import es from './es.yml';
+import fr from './fr.yml';
 
 export default i18n.use(LanguageDetector).init({
   resources: {
-    en, es,
+    en, es, fr,
   },
   fallbackLng: 'en', // Intentionally not lang
   defaultNS: 'common',
