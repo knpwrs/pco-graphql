0f6308e8e1f5ed0588650f85c258c8d23bbe5733
Kenneth Powers <ken@kenpowers.net>
2017-08-07 19:34:09 -0400

    Refactor components out of plans container.

---
+++ client/src/components/plans/light-text.js
@@ -0,0 +1,5 @@
+import g from 'glamorous';
+
+export default g.span({
+  opacity: 0.2,
+});
--- client/src/containers/plans.jsx
+++ client/src/containers/plans.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { gql, graphql } from 'react-apollo';
 import { compose } from 'recompose';
-import g, { Table } from 'glamorous';
 import { translate } from 'react-i18next';
 import Page from '../components/page';
-import Card from '../components/card';
+import ServiceType from '../components/plans/service-type';
 import { placeholderLoader } from '../components/loader';
+import { serviceTypeShape } from '../shapes/plans';
 
 const plansQuery = gql`
   query PlansQuery {
@@ -21,92 +21,6 @@ const plansQuery = gql`
     }
   }
 `;
-
-const planShape = PropTypes.shape({
-  id: PropTypes.string,
-  title: PropTypes.string,
-  dates: PropTypes.string,
-});
-
-const serviceTypeShape = PropTypes.shape({
-  id: PropTypes.string,
-  name: PropTypes.string,
-  plans: PropTypes.arrayOf(planShape),
-});
-
-const ColHead = g.th({
-  textAlign: 'left',
-  fontWeight: 300,
-});
-
-const DateCell = g.td({
-  width: '25%',
-  fontWeight: 300,
-});
-
-const TitleCell = g.td({
-  fontWeight: 200,
-});
-
-const PlanRow = g.tr((props, { highlightColor }) => ({
-  ':hover': {
-    backgroundColor: highlightColor,
-  },
-}));
-
-const LightText = g.span({
-  opacity: 0.2,
-});
-
-const BarePlan = ({ plan, t }) => (
-  <PlanRow>
-    <DateCell>{plan.dates}</DateCell>
-    <TitleCell>{plan.title || <LightText>{t('noTitle')}</LightText>}</TitleCell>
-  </PlanRow>
-);
-
-BarePlan.propTypes = {
-  plan: planShape.isRequired,
-  t: PropTypes.func.isRequired,
-};
-
-const Plan = translate('plans')(BarePlan);
-
-const BarePlanRows = ({ plans, t }) => (
-  <Table width="100%">
-    <thead>
-      <tr>
-        <ColHead>{t('date')}</ColHead>
-        <ColHead>{t('title')}</ColHead>
-      </tr>
-    </thead>
-    <tbody>
-      {plans.map(plan => <Plan key={plan.id} plan={plan} />)}
-    </tbody>
-  </Table>
-);
-
-BarePlanRows.propTypes = {
-  plans: PropTypes.arrayOf(planShape).isRequired,
-  t: PropTypes.func.isRequired,
-};
-
-const PlanRows = translate('plans')(BarePlanRows);
-
-const BareServiceType = ({ type, t }) => (
-  <Card title={type.name}>
-    {type.plans.length > 0
-      ? <PlanRows plans={type.plans} />
-      : <LightText>{t('noPlans')}</LightText>}
-  </Card>
-);
-
-BareServiceType.propTypes = {
-  type: serviceTypeShape.isRequired,
-  t: PropTypes.func.isRequired,
-};
-
-const ServiceType = translate('plans')(BareServiceType);
 
 const Plans = ({ data, t }) => (
   <Page title={t('plans')}>
