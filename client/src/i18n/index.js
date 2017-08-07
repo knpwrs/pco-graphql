7e908d254150ad77ee4b4238bc190d21c6d77489
Kenneth Powers <ken@kenpowers.net>
2017-08-07 16:56:14 -0400

    Add spanish translation.

--- client/src/i18n/index.js
+++ client/src/i18n/index.js
@@ -1,11 +1,15 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import en from './en.yml';
+import es from './es.yml';
 
 export default i18n.use(LanguageDetector).init({
   resources: {
-    en,
+    en, es,
   },
   fallbackLng: 'en', // Intentionally not lang
   defaultNS: 'common',
 });
+
+// Uncomment the following to test a language.
+// i18n.changeLanguage('en');
