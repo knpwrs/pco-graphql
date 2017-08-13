8e437e6c8f26c3ea115ef6fd19162b7ee8ce212f
Kenneth Powers <ken@kenpowers.net>
2017-08-13 18:57:56 -0400

    Add PhoneNumber and Email mutations.

--- server-rails/app/graphql/types/mutation_type.rb
+++ server-rails/app/graphql/types/mutation_type.rb
@@ -9,5 +9,27 @@ module Types
       description "Add a Person."
       resolve ->(obj, args, ctx) { Person.create(args[:attributes].to_h) }
     end
+
+    field :addPhoneNumber do
+      type PhoneNumberType
+      argument :personId, !types.ID
+      argument :attributes, !PhoneNumberInputType
+      description "Add a PhoneNumber to a Person."
+      resolve ->(obj, args, ctx) {
+        person = Person.find(args[:personId])
+        person.phone_numbers.create(args[:attributes].to_h)
+      }
+    end
+
+    field :addEmail do
+      type EmailType
+      argument :personId, !types.ID
+      argument :attributes, !EmailInputType
+      description "Add an Email to a Person."
+      resolve ->(obj, args, ctx) {
+        person = Person.find(args[:personId])
+        person.emails.create(args[:attributes].to_h)
+      }
+    end
   end
 end
